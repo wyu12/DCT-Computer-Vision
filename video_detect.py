1df1749f246a0353a412bfc3ea48a75f667e2d79
wyu12 <william.yu@duke.edu>
2019-10-11 18:45:42 -0400

    added a few lines for still image detection

--- video_detect.py
+++ video_detect.py
@@ -5,28 +5,33 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 detector = apriltag.Detector()
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+img = cv2.imread("data/apriltags.png", cv2.IMREAD_GRAYSCALE)
+#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+result = detector.detect(img)
+print(result)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    result = detector.detect(gray)
-    print(result)
-
-    # draw a bounding quadrilateral (optional)
-    if len(result) > 0:
-        pts = np.array(result[0].corners, dtype=np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(gray, [pts], True, (0, 0, 0), 3)
-
-    # display image
-    cv2.imshow('frame', gray)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#
+#     # Our operations on the frame come here
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     # Display the resulting frame
+#     result = detector.detect(gray)
+#     print(result)
+#
+#     # draw a bounding quadrilateral (optional)
+#     if len(result) > 0:
+#         pts = np.array(result[0].corners, dtype=np.int32)
+#         pts = pts.reshape((-1, 1, 2))
+#         cv2.polylines(gray, [pts], True, (255, 0, 0), 3)
+#
+#     # display image
+#     cv2.imshow('frame', gray)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
 # When everything done, release the capture
 cap.release()
