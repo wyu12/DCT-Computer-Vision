b94b10698127f94bf488fea95e5fd883c71ae649
wyu12 <william.yu@duke.edu>
2019-10-18 17:46:32 -0400

    generating output.jpg file

--- video_detect.py
+++ video_detect.py
@@ -5,10 +5,11 @@ import numpy as np
 cap = cv2.VideoCapture(0)
 detector = apriltag.Detector()
 
-img = cv2.imread("data/apriltags.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("data/2x6_tags.png", cv2.IMREAD_GRAYSCALE)
 #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-result = detector.detect(img)
+result,img2 = detector.detect(img, return_image = True)
 print(result)
+cv2.imwrite("output.jpg",img2)
 
 # while True:
 #     # Capture frame-by-frame
